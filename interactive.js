79cedecec30991fded3ec3d6301088f00b0a9320
Aditya J <ajain15@uic.edu>
2017-07-13 15:55:17 -0400

    rage quit and started from scratch

---
+++ interactive.js
@@ -0,0 +1,135 @@
+var getX;
+var getY;
+var getCircle;
+var xScaleRange;
+var yScaleRange;
+var circleScaleRange;
+var xScale;
+var yScale;
+var radiusScale;
+var countryList = []
+var q = d3.queue()
+    .defer(d3.csv, "data/circle.csv")
+    .defer(d3.csv, "data/xScale.csv")
+    .defer(d3.csv, "data/yScale.csv")
+    .awaitAll(function(error, results) {
+      if (error) throw error;
+        for(var i=0;i<results[0].length;i++){
+            countryList.push(results[0][i]["Country"]);
+        }
+        getCircle = getCountryDataFunc(results[0]);
+        circleScaleRange = getRange(results[0],parseInt);
+        getX = getCountryDataFunc(results[1]);
+        xScaleRange = getRange(results[1],parseFloat);
+        getY = getCountryDataFunc(results[2]);
+        yScaleRange = getRange(results[2],parseFloat);
+        makeChart();
+    });
+function getCountryDataFunc(data){
+    return function(country){
+        for(var i=0;i<data.length;i++){
+            if(data[i]["Country"]==country){
+                return data[i];
+            }
+        }
+    }
+}
+
+function getYear(year,object){
+    return parseInt(object[year]);
+}
+
+function getRange(data,parseFunction){
+    var max;
+    var min;
+    for(var i=0;i<data.length;i++){
+        for(var key in data[i]){
+            if(key!=="Country"){
+                var parsedNumber = parseFunction(data[i][key]);
+                if(max===undefined){
+                    max = parsedNumber;
+                    min = parsedNumber;
+                }
+                if(min>parsedNumber){
+                    min=parsedNumber;
+                }
+                if(max<parsedNumber){
+                    max=parsedNumber;
+                }
+            }
+        }
+    }
+    return [min,max];
+}
+
+function makeChart(){
+    var margin = {top: 19.5, right: 19.5, bottom: 19.5, left: 39.5};
+    var width = 960 - margin.right;
+    var height = 500 - margin.top - margin.bottom;
+
+    xScale = d3.scaleLinear().domain(xScaleRange).range([0, width]),
+    yScale = d3.scaleLinear().domain(yScaleRange).range([height, 0]),
+    radiusScale = d3.scaleSqrt().domain(circleScaleRange).range([0, 40]);
+// The x & y axes.
+var xAxis = d3.axisBottom(xScale).ticks(12, d3.format(",d")).tickSizeOuter(0),
+    yAxis = d3.axisLeft(yScale).tickSizeOuter(0);
+// Create the SVG container and set the origin.
+var svg = d3.select("#graph").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+    .attr("class", "gRoot");
+
+svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
+
+svg.append("g")
+    .attr("class", "y axis")
+    .call(yAxis);
+
+svg.append("g")
+    .attr("class","points")
+    .selectAll("circle")
+    .data(countryList.map(function(d){return [d,getX(d),getY(d),getCircle(d)];}))
+    .enter()
+    .append("circle")
+    .attr("id",function(d){return d[0];})
+    .attr("class","point")
+    .style("fill","red")
+    .attr("cx",function(d){return xScale(d[1]["1996"]);})
+    .attr("cy",function(d){return yScale(d[2]["1996"]);})
+    .attr("r",function(d){return radiusScale(d[3]["1996"])});
+}
+
+function update(year){
+    d3.selectAll(".point")
+        .transition()
+        .duration(1000)
+        .ease(d3.easeQuadInOut)
+        .attr("cx",function(d){return xScale(d[1][year]);})
+        .attr("cy",function(d){return yScale(d[2][year]);})
+        .attr("r",function(d){return radiusScale(d[3][year]);});
+}
+/*
+var margin = {top: 19.5, right: 19.5, bottom: 19.5, left: 39.5},
+    width = 960 - margin.right,
+    height = 500 - margin.top - margin.bottom;
+// Various scales. These domains make assumptions of data, naturally.
+var xScale = d3.scale.log().domain([300, 1e5]).range([0, width]),
+    yScale = d3.scale.linear().domain([10, 85]).range([height, 0]),
+    radiusScale = d3.scale.sqrt().domain([0, 5e8]).range([0, 40]),
+    colorScale = d3.scale.category10();
+// The x & y axes.
+var xAxis = d3.svg.axis().orient("bottom").scale(xScale).ticks(12, d3.format(",d")),
+    yAxis = d3.svg.axis().scale(yScale).orient("left");
+// Create the SVG container and set the origin.
+var svg = d3.select("#chart").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+    .attr("class", "gRoot")
+*/
