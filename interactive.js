746b338f9590ba00182807f37a9d4dddf782985b
Aditya J <ajain15@uic.edu>
2017-10-16 16:32:41 -0400

    the march of progress

--- interactive.js
+++ interactive.js
@@ -2,75 +2,138 @@ $( function() {
     $( "#accordion" ).accordion({
         heightStyle: "fill"
     });
-  } );
+});
 
-var getX;
-var getY;
-var getCircle;
-var xScaleRange;
-var yScaleRange;
-var circleScaleRange;
+var data;
+
 var xScale;
 var yScale;
+var xAxis;
+var yAxis;
 var radiusScale;
 var wbCode;
 var wbScale = {1:"#deebf7",2:"#c6dbef",3:"#9ecae1",4:"#6baed6",5:"#3182bd",6:"#08519c"}
-var countryList = []
+var countryList = new Set();
 var label;
-var currentYear = 1996;
+var currentYear = 1995;
+var currentX = "Perceived Rule Of Law";
+var currentY = "Regulatory Quality";
+var currentRadius = "GNI per Capita, PPP(ci$)";
+var width;
+var height;
 
-var q = d3.queue()
-    .defer(d3.csv, "data/circle.csv")
-    .defer(d3.csv, "data/xScale.csv")
-    .defer(d3.csv, "data/yScale.csv")
-    .defer(d3.csv, "data/wbcode.csv")
-    .awaitAll(function(error, results) {
-    if (error) throw error;
-    for(var i=0;i<results[0].length;i++){
-        countryList.push(results[0][i]["Country"]);
+var q = d3.csv("data/GDF_iLab.csv",function(result){
+    for(var i=0;i<result.length;i++){
+        countryList.add(result[i]["Country"]);
     }
-    getCircle = getCountryDataFunc(results[0]);
-    circleScaleRange = getRange(results[0],parseInt);
-    getX = getCountryDataFunc(results[1]);
-    xScaleRange = getRange(results[1],parseFloat);
-    getY = getCountryDataFunc(results[2]);
-    yScaleRange = getRange(results[2],parseFloat);
-    wbCode = getCountryDataFunc(results[3]);
+    var categories = Object.keys(result[0]);
+    
+    for(var i=0;i<categories.length;i++){
+        if(categories[i]!="Country"&&categories[i]!="Year"){
+            if(categories[i]=="Perceived Rule Of Law"){
+                d3.select("#dropdownX").append("option").html(categories[i]).attr("class","defaultoption");
+            }
+            else{
+                d3.select("#dropdownX").append("option").html(categories[i]);
+            }
+        }
+    }
+
+    for(var i=0;i<categories.length;i++){
+        if(categories[i]!="Country"&&categories[i]!="Year"){
+            if(categories[i]=="Regulatory Quality"){
+                d3.select("#dropdownY").append("option").html(categories[i]).attr("class","defaultoption");
+            }
+            else{
+                d3.select("#dropdownY").append("option").html(categories[i]);
+            }
+        }
+    }
+
+    for(var i=0;i<categories.length;i++){
+        if(categories[i]!="Country"&&categories[i]!="Year"){
+            if(categories[i]=="GNI per Capita, PPP(ci$)"){
+                d3.select("#dropdownR").append("option").html(categories[i]).attr("class","defaultoption");
+            }
+            else{
+                d3.select("#dropdownR").append("option").html(categories[i]);
+            }
+        }
+    }
+
+    var defaultoptions = document.getElementsByClassName("defaultoption");
+    for(var i=0;i<defaultoptions.length;i++){
+        defaultoptions[i].selected=true;
+    }
+    countryList = Array.from(countryList)
+    data = result;
     makeChart();
     makeBabyChart();
 });
 
-function getCountryDataFunc(data){
-    return function(country){
-        for(var i=0;i<data.length;i++){
-            if(data[i]["Country"]==country){
-                return data[i];
+/*
+
+*/
+
+
+
+function isEmpty(d,theScale,year){
+    return(d.filter(function(r){return r["Year"]==year;})[0][theScale]=="");
+}
+
+function getData(d,theScale,year){
+    if(d.filter(function(r){return r["Year"]==year;})[0][theScale]==""){
+        var yearIter = year - 1;
+        while(yearIter>=1995){
+            if(d.filter(function(r){return r["Year"]==yearIter;})[0][theScale]!=""){
+                //console.log(this);
+                return d.filter(function(r){return r["Year"]==yearIter;})[0][theScale];
             }
+            yearIter = yearIter - 1;
         }
+
+        yearIter = year + 1;
+        while(yearIter<=2015){
+            if(d.filter(function(r){return r["Year"]==yearIter;})[0][theScale]!=""){
+                
+                return d.filter(function(r){return r["Year"]==yearIter;})[0][theScale];
+            }
+            yearIter = yearIter + 1;
+        }
+
+        return getDomain(theScale)[0];
     }
+
+    return d.filter(function(r){
+        return r["Year"]==year;
+    })[0][theScale]; 
 }
 
 function getYear(year,object){
     return parseInt(object[year]);
 }
 
-function getRange(data,parseFunction){
+function getDomain(string){
+    return domainCalc(data.map(function(d){
+       return d[string]; 
+    }))
+}
+
+function domainCalc(data){
     var max;
     var min;
-    for(var i=0;i<data.length;i++){
-        for(var key in data[i]){
-            if(key!=="Country"){
-                var parsedNumber = parseFunction(data[i][key]);
-                if(max===undefined){
-                    max = parsedNumber;
-                    min = parsedNumber;
-                }
-                if(min>parsedNumber){
-                    min=parsedNumber;
-                }
-                if(max<parsedNumber){
-                    max=parsedNumber;
-                }
+    for(var i=0;i<data.length;i++){    
+        var parsedNumber = parseFloat(data[i]);
+        if(!isNaN(parsedNumber)){
+            if(max===undefined){
+                max = parsedNumber;
+                min = parsedNumber;
+            }
+            if(min>parsedNumber){
+                min=parsedNumber;
+            }
+            if(max<parsedNumber){
+                max=parsedNumber;
             }
         }
     }
@@ -140,18 +203,41 @@ function makeBabyChart(){
     });
 }
 
+function changeXScale(newData){
+    currentX = newData;
+    d3.select("#xLabel").text(newData);
+    xScale.domain(getDomain(newData)).range([0,width]);
+    d3.select("#xAxis").transition().duration(1500).ease(d3.easeQuadInOut).call(xAxis);
+    update(currentYear,false);   
+}
+
+function changeYScale(newData){
+    currentY=newData;
+    d3.select("#yLabel").text(newData);
+    yScale.domain(getDomain(newData)).range([height,0]);
+    d3.select("#yAxis").transition().duration(1500).ease(d3.easeQuadInOut).call(yAxis);
+    update(currentYear,false);  
+}
+
+function changeRadius(newData){
+    currentRadius = newData;
+    radiusScale.domain(getDomain(newData)).range([2, 40]);
+    update(currentYear,false); 
+}
+
 function makeChart(){
 
     d3.select("#theGraph").remove();
+    d3.select("#tooltips").remove();
     var margin = {top: 19.5, right: 19.5, bottom: 49.5, left: 25};
-    var width = document.getElementById('graph').clientWidth - margin.right;
-    var height = 500 - margin.top - margin.bottom;
+    width = document.getElementById('graph').clientWidth - margin.right;
+    height = 500 - margin.top - margin.bottom;
 
-    xScale = d3.scaleLinear().domain(xScaleRange).range([0, width]),
-    yScale = d3.scaleLinear().domain(yScaleRange).range([height, 0]),
-    radiusScale = d3.scaleSqrt().domain(circleScaleRange).range([2, 40]);
+    xScale = d3.scaleLinear().domain(getDomain(currentX)).range([0, width]),
+    yScale = d3.scaleLinear().domain(getDomain(currentY)).range([height, 0]),
+    radiusScale = d3.scaleSqrt().domain(getDomain(currentRadius)).range([2, 40]);
     // The x & y axes.
-    var xAxis = d3.axisBottom(xScale).ticks(12, d3.format(",d")).tickSizeOuter(0),
+    xAxis = d3.axisBottom(xScale).ticks(12, d3.format(",d")).tickSizeOuter(0);
     yAxis = d3.axisLeft(yScale).tickSizeOuter(0);
     // Create the SVG container and set the origin.
 
@@ -166,60 +252,98 @@ function makeChart(){
     .attr("id", "gRoot");
 
     svg.append("g")
-    .attr("class", "x axis")
+    .attr("id", "xAxis")
     .attr("transform", "translate(0," + height + ")")
     .call(xAxis);
 
     svg.append("g")
-    .attr("class", "y axis")
+    .attr("id", "yAxis")
     .call(yAxis);
 
     // Add an x-axis label.
     svg.append("text")
-    .attr("class", "x label")
+    .attr("id", "xLabel")
     .attr("text-anchor", "end")
     .attr("x", width)
     .attr("y", height - 6)
-    .text("Perceived Rule of Law");
+    .text(currentX);
     // Add a y-axis label.
     svg.append("text")
-    .attr("class", "y label")
+    .attr("id", "yLabel")
     .attr("text-anchor", "end")
     .attr("y", 6)
     .attr("dy", ".75em")
     .attr("transform", "rotate(-90)")
-    .text("Regulatory Quality");
+    .text(currentY);
     // Add the year label; the value is set on transition.
     label = svg.append("text")
     .attr("class", "year label")
     .attr("text-anchor", "end")
     .attr("y", height - 24)
     .attr("x", width)
-    .text(1996);
+    .text(1995);
 
     svg.append("g")
     .attr("id","points")
     .selectAll("circle")
-    .data(countryList.map(function(d){return [d,getX(d),getY(d),getCircle(d),wbCode(d).wbcode];}))
+    .data(countryList.map(function(d){
+       return data.filter(function(r){
+        return r["Country"]===d;
+       }) 
+    }))
     .enter()
     .append("circle")
-    .attr("id",function(d){return d[0].replace(/ /g,'');})
-    .attr("country",function(d){return d[0]})
+    .attr("id",function(d){return d[0]["Country"].replace(/ /g,'');})
     .attr("class","point")
-    .style("fill",function(d){return wbScale[d[4]]})
-    .attr("cx",function(d){return xScale(d[1]["1996"]);})
-    .attr("cy",function(d){return yScale(d[2]["1996"]);})
-    .attr("r",function(d){return radiusScale(d[3]["1996"])});
+    .attr("cx",function(d){
+        return xScale(getData(d,currentX,currentYear));
+    })
+    .attr("cy",function(d){
+        return yScale(getData(d,currentY,currentYear));
+    })
+    .attr("r",function(d){
+        return radiusScale(getData(d,currentRadius,currentYear)); 
+    })
+    .style("fill",function(d){
+        if(isEmpty(d,currentX,currentYear)||isEmpty(d,currentY,currentYear)||isEmpty(d,currentRadius,currentYear)){
+            return "red";
+        }
+        else{
+            return wbScale[d.filter(function(r){return r["Year"]==currentYear;})[0]["World Bank Classification"]];
+        }
+    });
     
     d3.selectAll(".point").select(function(d){
         var boundingClientRect = this.getBoundingClientRect();
-        d3.select("#tooltips")
+        var theTooltip = d3.select("#tooltips")
         .append("div")
         .attr("class","tooltip")
-        .attr("id",d[0].replace(/ /g,'')+"tooltip")
+        .attr("id",d[0]["Country"].replace(/ /g,'')+"tooltip")
         .style("top",boundingClientRect.top+boundingClientRect.height)
-        .style("left",boundingClientRect.left+boundingClientRect.width)
-        .text(d[0]);
+        .style("left",boundingClientRect.left+boundingClientRect.width);
+
+
+        theTooltip.append('div')
+        .attr("id",d[0]["Country"].replace(/ /g,'')+"tooltipcaption")
+        .attr("class","tooltipcaption")
+        .text(d[0]["Country"]);
+
+        theTooltip.append('div')
+        .attr("class","tooltipdetail")
+        .attr("id",d[0]["Country"].replace(/ /g,'')+"tooltipX")
+        .text(currentX+": "+(isEmpty(d,currentX,currentYear)?"No Data":getData(d,currentX,currentYear)));
+
+        theTooltip.append('div')
+        .attr("class","tooltipdetail")
+        .attr("id",d[0]["Country"].replace(/ /g,'')+"tooltipY")
+        .text(currentY+": "+getData(d,currentY,currentYear));
+
+        theTooltip.append('div')
+        .attr("class","tooltipdetail")
+        .attr("id",d[0]["Country"].replace(/ /g,'')+"tooltipRadius")
+        .text(currentRadius+": "+getData(d,currentRadius,currentYear));
+
+        //theTooltip.remove();
     });
 
     reorder();
@@ -230,8 +354,7 @@ function attachListeners(){
         if(this.checked){
             d3.select("#"+this.getAttribute('country'))
             .on("mouseenter",function(d){
-                d3.select("#tempCirc").remove();
-                d3.select("#tempLabel").remove();
+
 
                 d3.select("#legend"+d[4]).style("border","2px solid #E8336D");
                 d3.select(this).style("stroke","#E8336D");
@@ -261,7 +384,7 @@ function attachListeners(){
                 .style("opacity",0.3);
             })
             .on("mouseleave",function(d){
-                d3.select("#legend"+d[4]).style("border",null);
+
                 d3.select(this).style("stroke","black");
                 d3.select(this).style("stroke-width","1px");
                 d3.select("#tempCirc").remove();
@@ -271,15 +394,17 @@ function attachListeners(){
             });
         }
         else{
+
             d3.select("#"+this.getAttribute('country'))
             .on("mouseenter",function(d){
-                d3.select("#tempCirc").remove();
-                d3.select("#tempLabel").remove();
+                d3.select("#"+this.id+"tooltipX").style("display","block");
+                d3.select("#"+this.id+"tooltipY").style("display","block");
+                d3.select("#"+this.id+"tooltipRadius").style("display","block");
 
-                d3.select("#"+this.getAttribute('id')+"tooltip")
+                d3.select("#"+this.id+"tooltip")
                 .style("display","block");
             
-                d3.select("#legend"+d[4]).style("border","2px solid #E8336D");
+                d3.select("#legend"+d[0]["Country"]).style("border","2px solid #E8336D");
                 d3.select(this).style("stroke","#E8336D");
                 d3.select(this).style("stroke-width","3px");
 
@@ -307,10 +432,14 @@ function attachListeners(){
                 .style("opacity",0.3);
             })
             .on("mouseleave",function(d){
+                d3.select("#"+this.id+"tooltipX").style("display","none");
+                d3.select("#"+this.id+"tooltipY").style("display","none");
+                d3.select("#"+this.id+"tooltipRadius").style("display","none");
+
                 d3.select("#"+this.getAttribute('id')+"tooltip")
                 .style("display","none");
 
-                d3.select("#legend"+d[4]).style("border",null);
+                d3.select("#legend"+d[0]["Country"]).style("border",null);
                 d3.select(this).style("stroke","black");
                 d3.select(this).style("stroke-width","1px");
 
@@ -328,10 +457,6 @@ function yearChange(year){
 }
 
 function update(year,playButton){
-    d3.selectAll(".point")
-    .attr("cx",function(d){return xScale(d[1][year]);})
-    .attr("cy",function(d){return yScale(d[2][year]);})
-    .attr("r",function(d){return radiusScale(d[3][year]);});
 
     var t = d3.transition()
     .duration(1000)
@@ -347,36 +472,77 @@ function update(year,playButton){
     });
 
     d3.selectAll(".point")
-    .select(function(d){
-        var boundingClientRect = this.getBoundingClientRect();
-        
-        d3.select("#"+d[0].replace(/ /g,'')+"tooltip")
-        .transition(t)
-        .style("top",boundingClientRect.bottom)
-        .style("left",boundingClientRect.right);
+    .attr("cx",function(d){
+        return xScale(getData(d,currentX,year));
+    })
+    .attr("cy",function(d){
+        return yScale(getData(d,currentY,year)); 
+    })
+    .attr("r",function(d){
+        return radiusScale(getData(d,currentRadius,year)); 
     });
 
     d3.selectAll(".point")
-    .attr("cx",function(d){return xScale(d[1][currentYear]);})
-    .attr("cy",function(d){return yScale(d[2][currentYear]);})
-    .attr("r",function(d){return radiusScale(d[3][currentYear]);});
+    .select(function(d){
+        var boundingClientRect = this.getBoundingClientRect();
+        
+        d3.select("#"+d[0]["Country"].replace(/ /g,'')+"tooltip")
+        .transition(t)
+        .style("top",boundingClientRect.bottom)
+        .style("left",boundingClientRect.right);
+    
+    });
+
+    d3.selectAll(".point")
+    .attr("cx",function(d){
+        return xScale(getData(d,currentX,currentYear));
+    })
+    .attr("cy",function(d){
+        return yScale(getData(d,currentY,currentYear));
+    })
+    .attr("r",function(d){
+        return radiusScale(getData(d,currentRadius,currentYear)); 
+    })
+    .style("fill",function(d){
+        if(isEmpty(d,currentX,year)||isEmpty(d,currentY,year)||isEmpty(d,currentRadius,year)){
+            return "red";
+        }
+        else{
+            return wbScale[d.filter(function(r){return r["Year"]==year;})[0]["World Bank Classification"]];
+        }
+    });
 
     d3.selectAll(".point")
     .transition(t)
-    .attr("cx",function(d){return xScale(d[1][year]);})
-    .attr("cy",function(d){return yScale(d[2][year]);})
-    .attr("r",function(d){return radiusScale(d[3][year]);});
+    .attr("cx",function(d){
+        return xScale(getData(d,currentX,year));
+    })
+    .attr("cy",function(d){
+        return yScale(getData(d,currentY,year));
+    })
+    .attr("r",function(d){
+        return radiusScale(getData(d,currentRadius,year));
+    });
+    
+    d3.selectAll(".point").select(function(d){
+        d3.select("#"+d[0]["Country"].replace(/ /g,'')+"tooltipX")
+        .text(currentX+": "+(isEmpty(d,currentX,year)?"No Data":getData(d,currentX,year)));
+        
+        d3.select("#"+d[0]["Country"].replace(/ /g,'')+"tooltipY")
+        .text(currentY+": "+(isEmpty(d,currentY,year)?"No Data":getData(d,currentY,year)));
 
-    document.getElementById('slider').value = year;
-    currentYear = document.getElementById('slider').value;
+        d3.select("#"+d[0]["Country"].replace(/ /g,'')+"tooltipRadius")
+        .text(currentRadius+": "+(isEmpty(d,currentRadius,year)?"No Data":getData(d,currentRadius,year)));
+    });
 
+    document.getElementById('slider').value = parseInt(year);
+    currentYear = parseInt(document.getElementById('slider').value);
     label.text(year);
 }
 
 function play(){
-    
     if(parseInt(document.getElementById('slider').value)==2015){
-        update(1996,true);        
+        update(1995,true);        
     }
     else{
         update(parseInt(document.getElementById('slider').value)+1,true);
@@ -416,5 +582,16 @@ function resizefunc(){
     makeChart();
     makeBabyChart();
 };
+
+/*
+for(let country of mySet.values()){
+    console.log(country);
+    for(let y of year.values()){
+        let theRecord = data.filter(function(d){return d["Country"]==country&&d["Year"]==y});
+        console.log(","+theRecord["%ofWomenFormallyEmployed"]);
+    }
+    console.log("\n");
+}
+*/
 
 //420 blaze it
